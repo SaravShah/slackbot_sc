1e3e00071b08e71170457a34af3904f2b9676066
Sarav Shah <saravsshah@gmail.com>
2018-03-14 13:13:57 -0700

    Send RTM to slack channel

---
+++ index.js
@@ -0,0 +1,29 @@
+const { RTMClient } = require('@slack/client');
+// const { CLIENT_EVENTS } = require('@slack/client');
+// // var RtmClient = require('@slack/client').RtmClient;
+// var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var rtm = new RTMClient(env.TOKEN);
+rtm.start();
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+const conversationId = 'C9QR594F8';
+
+// The RTM client can send simple string messages
+rtm.sendMessage('Hello there', conversationId)
+  .then((res) => {
+    // `res` contains information about the posted message
+    console.log('Message sent: ', res.ts);
+  })
+  .catch(console.error);
+
+
+// let channel;
+// rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+//   for (const c of rtmStartData.channels) {
+//       if (c.is_member && c.name ==='test-shared-config') { channel = c.id }
+//   }
+//   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}`);
+// });
+// rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
+//   rtm.sendMessage("Hello!", channel);
+// });
